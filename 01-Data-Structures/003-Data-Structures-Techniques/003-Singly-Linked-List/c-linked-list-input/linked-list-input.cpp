5956ff405cf5e12f17d1821e412b94fc1ed49046
Faizul-Bitto <fibhuiyanbitto06@gmail.com>
2025-12-24 19:25:52 +0600

    Add implementation of a singly linked list in C++, including input and output files for testing, and a VSCode configuration for development.

---
+++ 01-Data-Structures/003-Data-Structures-Techniques/003-Singly-Linked-List/c-linked-list-input/linked-list-input.cpp
@@ -0,0 +1,64 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node
+{
+public:
+    int value;
+    Node *next;
+
+    Node(int value)
+    {
+        this->value = value;
+        this->next = NULL;
+    }
+};
+
+void insertValueAtTail(Node *&head, Node *&tail, int value)
+{
+    Node *newNode = new Node(value);
+
+    if (head == NULL)
+    {
+        head = newNode;
+        tail = newNode;
+
+        return;
+    }
+
+    tail->next = newNode;
+    tail = newNode;
+}
+
+void printLinkedList(Node *head)
+{
+    Node *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->value << " ";
+        temp = temp->next;
+    }
+}
+
+int main()
+{
+    Node *head = NULL;
+    Node *tail = NULL;
+
+    int value;
+    while (true)
+    {
+        cin >> value;
+        if (value == -1)
+        {
+            break;
+        }
+
+        insertValueAtTail(head, tail, value);
+    }
+
+    printLinkedList(head);
+
+    return 0;
+}
